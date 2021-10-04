aa81b71701dc44336c0a1d5c78ab8e5b74fb3304
Auralius Manurung <auralius@users.noreply.github.com>
2021-10-04 19:06:09 +0700

    Sutton's mountain car is added

---
+++ demo_07_mountain_car.m
@@ -0,0 +1,90 @@
+% Auralius Manurung
+% ME - Universitas Pertamina
+% 2021
+
+clear
+close all
+clc
+
+% Setup the states and the inputs
+P = ( -1.2  : 0.001 : 0.5)';
+V = ( -0.07 : 0.001 : 0.07)';
+U = [-1 0 1]';
+
+% Setup the horizon
+n_horizon = 100;
+
+% Initiate the solver
+dps = dps_2X_1U(P, V, U, n_horizon, @state_update_fn, @stage_cost_fn, ...
+                @terminal_cost_fn);
+
+% Extract meaningful results
+dps = trace_2X_1U(dps, -0.5, 0);
+
+% Do plotting here
+plot_2X_1U(dps, '-');
+
+visualize(dps);
+
+%%
+function [p_next, v_next] = state_update_fn(p, v, u)
+v_next = v + 0.001*u - 0.0025*cos(3*p);
+p_next = p + v;
+
+% Left wall is inelastic
+if p_next <= -1.2
+    v_next = 0;
+end
+end
+
+%%
+function J = stage_cost_fn(x1, x2, u, k)
+
+J = ones(size(u));
+
+end
+
+%%
+function J = terminal_cost_fn(x1, x2)
+% Weighting factors
+a2 = 10;
+a3 = 10;
+
+% Final states
+xf = 0.5;
+vf = 0;
+
+J = a2.*(x1-xf).^2 + a3.*(x2-vf).^2;
+end
+
+%%
+function visualize(dps)
+hfig = figure;
+hold on;
+
+% Draw y = sine(3p)to representate a mountain
+p = -1.2:0.001:0.5;
+plot(p, sin(3 * p));    
+set(gca, 'YTickLabel', [ ]); 
+
+htext1 = text(-0.7,0.5, '');
+htext2 = text(-0.7,0.4, '');
+
+%% Animate the car
+car = plot(0,0, 'or', 'LineWidth', 4);
+for k = 1 : dps.n_horizon
+    set(car, 'XData', dps.x1_star(k));
+    
+    % Plug the XStar to the equation of the mountain
+    set(car, 'YData', sin(3*dps.x1_star(k))); 
+    
+    htext1.String = ['Stage-', num2str(k), ', x = ', num2str(dps.x1_star(k)),', v = ', num2str(dps.x2_star(k))] ;
+    
+    if k < dps.n_horizon % input is one-data-point shorter
+        htext2.String = ['u = ', num2str(dps.u_star(k))];
+    end
+    
+    write2gif(hfig, k, 'mountain_car.gif');
+    drawnow; 
+end
+end
