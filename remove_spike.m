0f244375389b60ffb66b8a18660a02115607a82d
Auralius Manurung <auralius@users.noreply.github.com>
2021-10-04 15:48:54 +0700

    Documentation works

--- remove_spike.m
+++ remove_spike.m
@@ -1,5 +1,7 @@
+% Remove spike whose width is only one stage
+
 function x = remove_spike(x)
-if length(x) > 10
+if length(x) > 10 % at least 10 stages
     [~,loc] = findpeaks(x, 'Threshold',1);
     if ~isempty(loc)
         x(loc) = NaN;
--- snap.m
+++ snap.m
@@ -1,7 +1,7 @@
-%%
-% This function will convert a given value to closest integer ranging
-% from 1 to N+1.
-% minVal will be converted to 1 and maxVal will be converted to N+1.
+% Convert a given value to closest integer ranging from 1 to N+1
+%
+% min_val will be converted to 1 and max_val will be converted to N+1.
+%
 
 function snapped_val = snap(val, min_val, max_val, N)
 
