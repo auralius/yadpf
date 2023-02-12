1ae43e51b8979528602aa2c7de8649abe240fdf9
Auralius Manurung <auralius@users.noreply.github.com>
2023-02-12 09:17:30 +0700

    Fixed: boundary costs

--- src/yadpf_solve.m
+++ src/yadpf_solve.m
@@ -9,16 +9,16 @@ function dpf = yadpf_solve(dpf)
 % Outputs:
 %    dpf - The same data structure as the input, but with the results
 %
-% Other m-files required: 
-%   fastsca2mat.m 
-%   snap.m 
+% Other m-files required:
+%   fastsca2mat.m
+%   snap.m
 %   fastreprowvec.m
 %   fastrepcolvec.m
 %   fastsub2ind2.m
 %
 % Author:
 %   Auralius Manurung
-%   Universitas Pertamina 
+%   Universitas Pertamina
 %   auralius.manurung@ieee.org
 
 %------------- BEGIN CODE --------------
@@ -59,7 +59,7 @@ U = generate_U_for_all_X();
 fprintf('Simulating the system one step ahead...\n');
 fprintf('Progress: ')
 
-n_iter = dpf.T_ocp / dpf.T_dyn; 
+n_iter = dpf.T_ocp / dpf.T_dyn;
 
 ll = 0;
 for i = 1 : 1 : n_iter
@@ -72,9 +72,9 @@ end
 fprintf('\nApplying the boundaries...\n');
 r = cell(1, n_states);
 infeas = cell(1, n_states);
-for i = 1 : n_states   
-    [r{i}, infeas{i}] = snap(X{i}, lb(i), ub(i), nX(i));    
-end 
+for i = 1 : n_states
+    [r{i}, infeas{i}] = snap(X{i}, lb(i), ub(i), nX(i));
+end
 
 next_ind = flexible_sub2ind(nX, r{:});
 
@@ -83,7 +83,7 @@ clear X r; % We are done with X and r
 % Find next states that are outside the boudaries, preset a very high cost!
 J_bounds = zeros(size(J));
 for i = 1 : n_states
-    J_bounds(next_ind(ind2sub(nX, infeas{i}))) = LARGE_NUMBER;
+    J_bounds(next_ind(ind2sub(size(next_ind), infeas{i}))) = LARGE_NUMBER;
 end
 
 clear infeas; % We are done with infeasible
@@ -106,21 +106,21 @@ ll = 0;
 for k = dpf.n_horizon-1 : -1 : 1
     fprintf(repmat('\b',1,ll));
     ll = fprintf('%i',k);
-    
+
     J_old = J;
-          
+
     [J_min, J_min_idx] = min(dpf.stage_cost_fn(X, U, k, dpf.T_ocp) + ...
                              J_old(next_ind) + J_bounds(next_ind), [], 1);
-      
+
     descendant_matrix(k,:) = next_ind(fastsub2ind2([nUU nXX], ...
                                       J_min_idx, 1:nXX))';
-    
+
     b = flexible_ind2sub(nU, J_min_idx);
     for i = 1 : n_inputs
-        U_star_matrix{i}(k,:) = b{i};    
+        U_star_matrix{i}(k,:) = b{i};
     end
 
-    J(1:nXX) = J_min;    
+    J(1:nXX) = J_min;
 end
 
 fprintf('\nComplete!\n')
@@ -149,17 +149,17 @@ dpf.method   = 'dpa';
         sub = flexible_ind2sub(nX, 1:nXX);
         X = cell(1, n_states);
         for n = 1 : n_states
-            X{n} = dpf.states{n}(sub{n});            
+            X{n} = dpf.states{n}(sub{n});
         end
         clear sub;
     end
 
     function X = generate_X_for_all_U()
-    % Generate matrix X: nUU times nXX    
+    % Generate matrix X: nUU times nXX
         sub = flexible_ind2sub(nX, fastreprowvec(1:nXX, nUU));
         X = cell(1, n_states);
         for n = 1 : n_states
-            X{n} = dpf.states{n}(sub{n});            
+            X{n} = dpf.states{n}(sub{n});
         end
         clear sub;
     end
